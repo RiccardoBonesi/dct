ac99bac1adbc4cf4d401f4a0198647266aad2085
Riccardo Bonesi <riccardo.bonesi@gmail.com>
2018-05-21 14:42:22 +0200

    dct matrice prof

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy.fftpack as fp
 import math
 
+from scipy import fftpack
 
 def my_dct(array):
     size = array.size
@@ -21,6 +22,37 @@ def my_dct(array):
     return result
 
 
+def my_dct_2d(matrix):
+    first_dct = np.apply_along_axis(my_dct, axis=1, arr=matrix)
+    trasposed_matrix = first_dct.transpose()
+    second_dct = np.apply_along_axis(my_dct, axis=1, arr=trasposed_matrix)
+
+    return second_dct.transpose()
+
+
+def fftdct_wrapper(matrix):
+    return fftpack.dct(matrix, norm='ortho')
+
+
+def fftidct_wrapper(matrix):
+    return fftpack.idct(matrix, norm='ortho')
+
+
+def fft_dct_2d(matrix):
+    first_dct = np.apply_along_axis(fftdct_wrapper, axis=1, arr=matrix)
+    trasposed_matrix = first_dct.transpose()
+    second_dct = np.apply_along_axis(fftdct_wrapper, axis=1, arr=trasposed_matrix)
+
+    return second_dct.transpose()
+
+
+def fft_idct_2d(matrix):
+    first_dct = np.apply_along_axis(fftidct_wrapper, axis=1, arr=matrix)
+    trasposed_matrix = first_dct.transpose()
+    second_dct = np.apply_along_axis(fftidct_wrapper, axis=1, arr=trasposed_matrix)
+
+    return second_dct.transpose()
+
 def validate():
     array = np.array([231, 32, 233, 161, 24, 71, 140, 245])
 
@@ -28,21 +60,17 @@ def validate():
 
     print(result)
 
-    matrix = np.matrix("231, 32, 233, 161, 24, 71, 140, 245; "
-                          "247, 40, 248, 245, 124, 204, 36, 107;"
-                          "234, 202, 245, 167, 9, 217, 239, 173;"
-                          "193, 190, 100, 167, 43, 180, 8, 70;"
-                          "11, 24, 210, 177, 81, 243, 8, 112;"
-                          "97, 195, 203, 47, 125, 114, 165, 181;"
-                          "193, 70, 174, 167, 41, 30, 127, 245;"
-                          "87, 149, 57, 192, 65, 129, 178, 228")
+    matrix = np.array([[231, 32, 233, 161, 24, 71, 140, 245], [247, 40, 248, 245, 124, 204, 36, 107],
+                      [234, 202, 245, 167, 9, 217, 239, 173], [193, 190, 100, 167, 43, 180, 8, 70],
+                      [11, 24, 210, 177, 81, 243, 8, 112], [97, 195, 203, 47, 125, 114, 165, 181],
+                      [193, 70, 174, 167, 41, 30, 127, 245], [87, 149, 57, 192, 65, 129, 178, 228]])
 
-    # dct2_result = my_dct_2d(matrix)
-    # print(dct2_result)
-    # py_dct2_result = fft_dct_2d(matrix)
-    # print(py_dct2_result)
-    #
-    # print(numpy.allclose(dct2_result, py_dct2_result))
+    dct2_result = my_dct_2d(matrix)
+    print(dct2_result)
+    py_dct2_result = fft_dct_2d(matrix)
+    print(py_dct2_result)
+
+    print(np.allclose(dct2_result, py_dct2_result))
 
 
 if __name__ == "__main__":
