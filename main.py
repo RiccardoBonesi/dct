144a9014d71c3278551345b64aea1002b74d9670
Riccardo Bonesi <riccardo.bonesi@gmail.com>
2018-05-21 15:54:41 +0200

    prima parte finita (forse)

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from PIL import Image
+# from PIL import Image
 import numpy as np
-import scipy.fftpack as fp
 import math
-
+import time
+import cv2
 from scipy import fftpack
+
 
 def my_dct(array):
     size = array.size
@@ -53,6 +54,7 @@ def fft_idct_2d(matrix):
 
     return second_dct.transpose()
 
+
 def validate():
     array = np.array([231, 32, 233, 161, 24, 71, 140, 245])
 
@@ -60,43 +62,44 @@ def validate():
 
     print(result)
 
-    matrix = np.array([[231, 32, 233, 161, 24, 71, 140, 245], [247, 40, 248, 245, 124, 204, 36, 107],
+    test = np.array([[231, 32, 233, 161, 24, 71, 140, 245], [247, 40, 248, 245, 124, 204, 36, 107],
                       [234, 202, 245, 167, 9, 217, 239, 173], [193, 190, 100, 167, 43, 180, 8, 70],
                       [11, 24, 210, 177, 81, 243, 8, 112], [97, 195, 203, 47, 125, 114, 165, 181],
                       [193, 70, 174, 167, 41, 30, 127, 245], [87, 149, 57, 192, 65, 129, 178, 228]])
 
-    dct2_result = my_dct_2d(matrix)
+    dct2_result = my_dct_2d(test)
     print(dct2_result)
-    py_dct2_result = fft_dct_2d(matrix)
+    py_dct2_result = fft_dct_2d(test)
     print(py_dct2_result)
 
     print(np.allclose(dct2_result, py_dct2_result))
 
 
 if __name__ == "__main__":
-    img = Image.open("big_tree.bmp")
-    # img.show()
+    # load image
+    # img = Image.open("big_tree.bmp")
+    img = cv2.imread('artificial.bmp', 0)
 
-    validate()
+    # test matrix prof
+    # validate()
 
-    # array = np.array(img)
-    #
-    # data = np.array([[231, 32, 233, 161, 24, 71, 140, 245], [247, 40, 248, 245, 124, 204, 36, 107],
-    #                  [234, 202, 245, 167, 9, 217, 239, 173], [193, 190, 100, 167, 43, 180, 8, 70],
-    #                  [11, 24, 210, 177, 81, 243, 8, 112], [97, 195, 203, 47, 125, 114, 165, 181],
-    #                  [193, 70, 174, 167, 41, 30, 127, 245], [87, 149, 57, 192, 65, 129, 178, 228]])
-    #
-    # x = np.array([[1, 2], [3, 4]])
-    #
-    # im2freq = lambda data: fp.rfft(fp.rfft(data, axis=0), axis=1)
-    # freq2im = lambda f: fp.irfft(fp.irfft(f, axis=1), axis=0)
-    #
-    # freq = im2freq(data)
-    # back = freq2im(freq)
-    #
-    # print(freq)
-    # print(back)
+    matrix = np.array(img)
 
+    start = time.time()
+    my_result = my_dct_2d(matrix)
+    end = time.time()
+    my_elapsed = round(end - start, 4)
+    start = time.time()
+    fft_result = fft_dct_2d(matrix)
+    end = time.time()
+    fft_elapsed = round(end - start, 4)
+
+    similar_result = np.allclose(my_result, fft_result)
+    print("MyDCT elapsed: {} sec - FFT_DCT elapsed: {} sec - Results are similar: {}"
+          .format(my_elapsed, fft_elapsed, similar_result))
+
+
+    print('ok')
 
 
 
