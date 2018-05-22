526b1335926ee0a7da21094717638e677a073d1a
Riccardo Bonesi <riccardo.bonesi@gmail.com>
2018-05-22 12:16:38 +0200

    grafica

--- dct_GUI.py
+++ dct_GUI.py
@@ -1,26 +1,52 @@
 # C:\Users\ricca\Desktop\artificial.bmp
 from tkinter import *
 import cv2
-import numpy as np
-
+from fft import *
 
 
 def callback():
-    img_path = E1.get()
-    print(img_path)
+    img_path = Path_entry.get()
+    D = int(D_entry.get())                  # TODO controlli valore
     img = cv2.imread(img_path, 0)
-    matrix = np.array(img, dtype=float)
+    matrix = np.array(img, dtype=float)     # array f
+    cols = matrix.shape[0]
+    rows = matrix.shape[1]
+    fft_result = fft_dct_2d(matrix)         # array c
     print('You clicked the button!')
+    print(img_path)
+    print(D)
     print(matrix)
 
-top = Tk()
-L1 = Label(top, text="Image Path: ")
-L1.grid(row=0, column=0)
-E1 = Entry(top, bd=5)
-E1.grid(row=0, column=1)
 
-MyButton1 = Button(top, text="Submit", width=10, command=callback)
-MyButton1.grid(row=1, column=1)
+top = Tk()
+
+label_transparent = Label(top, text='')
+label_transparent.grid(row=0, column=0)
+
+# PATH
+Path_label = Label(top, text="Image Path: ")
+Path_label.grid(row=1, column=0)
+Path_entry = Entry(top, bd=5, width=50)
+Path_entry.grid(row=1, column=1)
+
+# D VALUE
+D_label = Label(top, text="Value d: ")
+D_label.grid(row=2, column=0)
+D_entry = Entry(top, bd=5)
+D_entry.grid(row=2, column=1, sticky='we')
+
+
+# B VALUE
+beta_label = Label(top, text="Value \u03B2: ")
+beta_label.grid(row=3, column=0)
+beta_entry = Entry(top, bd=5)
+beta_entry.grid(row=3, column=1, sticky='we')
+
+MyButton1 = Button(top, text="Submit", width=20, height=3, command=callback)
+MyButton1.grid(row=2, column=4)
+
+top.grid_columnconfigure(4, minsize=200)
+top.grid_rowconfigure(4, minsize=50)
 
 
 
